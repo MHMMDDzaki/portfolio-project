92266bb57a5bae50e4eee6061ee421dfa6ce0c11
MHMMDDzaki <dzakibs@gmail.com>
2023-06-20 18:50:41 +0700

    template

---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Home(){
+    return <>
+        <nav className="w-100">
+            <div className="flex">
+                <div>
+                    <h3>Portfolio.app</h3>
+                </div>
+                <div>
+                    <p>Item 1</p>
+                    <p>Item 2</p>
+                    <p>Item 3</p>
+                </div>
+            </div>
+        </nav>
+    </>
+}
+
+export default Home
